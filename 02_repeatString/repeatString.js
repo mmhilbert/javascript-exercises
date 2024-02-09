8598863ec2e2640271350b244d0b0bdcca375c82
Megan Hilbert <meganmhilbert@gmail.com>
2024-02-09 17:38:59 -0600

    completed javascript exercises 1 and 2 in JavaScript Fundamentals.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let temp = ""
+    for (let i=0; i<num; i++) {
+        temp = temp + string; 
+    }
 
+    return temp;
+    
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
