e34ba7d7f59d086909820e4820ad01983d1aa389
Megan Hilbert <meganmhilbert@gmail.com>
2024-02-11 13:26:24 -0600

    Completed exercise 3 Reverse String.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
 
+const reverseString = function(string) {
+    /* turn string into array 
+    reverse the array 
+    put reversed array together 
+    return output */
+
+    let tempArray = string.split("") 
+    let reverseArray = []
+    for (let i = 1; i <= tempArray.length; i++) {
+        reverseArray.push(tempArray[tempArray.length-i])
+    }
+
+    return reverseArray.join("")
 };
 
 // Do not edit below this line
