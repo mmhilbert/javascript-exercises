ee9b8493031c901c1f4a82977ef0c6e4c2205ef1
Megan Hilbert <meganmhilbert@gmail.com>
2024-02-12 14:47:07 -0600

    Completed exercise 5 Sum All in JavaScript Exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    /* list out all numbers between first and last number 
+       add all listed numbers together
+       return final sum of numbers 
+    */
+   /* if first or second parameter are not a number, return ERROR
+      if first or second parameter are less than 0, return ERROR  */
+
+    if (typeof firstNum != 'number' || typeof lastNum != 'number') {
+        return "ERROR"
+    }  
+    if (firstNum < 0 || lastNum < 0) {
+        return "ERROR"
+    }
+
+    let smallerNum = 0;
+    let largerNum = 0;
+    
+    if (lastNum > firstNum) {
+        smallerNum = firstNum;
+        largerNum = lastNum;
+    } else {
+        smallerNum = lastNum;
+        largerNum = firstNum;
+    }
+    
+    let newSum = 0;
+       for (let i = smallerNum; i <= largerNum; i++) {
+        newSum += i;
+       }
+    return newSum;
 
 };
 
