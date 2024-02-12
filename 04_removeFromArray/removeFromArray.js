29f8ad61d59f4a6c911b7e6db42670bf3cbeb0a4
Megan Hilbert <meganmhilbert@gmail.com>
2024-02-12 12:45:13 -0600

    Completed exercise 4 Remove From Array in JS Exercises.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+let testArray = []
+let remove = []
+
+const removeFromArray = function(...args) {
+   const myArray = args.shift()
+   return myArray.filter(function(item){
+    return !args.includes(item)
+   })
 
 };
+
+removeFromArray(testArray, remove)
 
 // Do not edit below this line
 module.exports = removeFromArray;
